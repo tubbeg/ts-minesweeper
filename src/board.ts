b8e362c81cb576db26160bc865052de32dc61d96
Tobias Gärdebro <tubbeg@gmail.com>
2024-12-03 18:42:11 +0100

    Created table with React. Styling is working with Bulma CSS. Added callback functions. Need to begin working on game logic

--- src/board.ts
+++ src/board.ts
@@ -3,7 +3,9 @@
 type Board = {x: number, y: number}
 
 
-export function makeBoard (x: number,y:number)
+export function makeBoard (x: number,y:number) : Board
 {
     return {x: x, y: y};
 }
+
+
--- src/game.tsx
+++ src/game.tsx
@@ -3,26 +3,57 @@ import { createRoot } from 'react-dom/client'
 import { makeBoard } from "./board"
 
 
-function MyButton() {
-    return (
-      <button>Click here</button>
-    )
-  }
+function stuff (x : number,y : number) {
+    
+}
+
+type TUpdateState = (x:number,y:number) => void;
 
 
-type Stuff =
-  | {some: number}
-  | "stuff"
+function createCells (x : number, y : number, cellCb : TUpdateState){
+    return Array.from(
+        { length: x },
+        (_, i) => (
+            <td key={"uniqueId" + i.toString() + y.toString()} >
+                <button onClick={(_) => {cellCb(i,y)}} className="button">{i.toString() + y.toString()}</button>
+            </td>
+        )
+    );    
+}
+
+
+function createRows (x : number, y : number, cb : TUpdateState){
+    return Array.from(
+        { length: y },
+        (_, i) => (
+            <tr key={"uniqueId" + i.toString() + y.toString()} >
+                {createCells(x,i,cb)}
+            </tr>
+        )
+    );    
+}
+
+const reactTable = (rows : number, cols : number, cb : TUpdateState) => {
+    return (<div>
+        <table className="table">
+            <tbody>
+            {createRows(rows, cols, cb)}
+            </tbody>
+        </table>
+    </div>)
+}
+
+function updateStateFunction (x:number,y:number){
+    console.log(x,y)
+}
 
 export default function MyApp() {
-    const stuff : Stuff = {some: 1337}
     const myBool = true
     const myBoard = makeBoard(5,5)
     return (
-        <div>
-            {stuff.some}
-        <h1>Minesweeper</h1>
-        <MyButton />
+        <div className="centerDiv">
+        <h1 className="box h2 has-text-primary"> Minesweeper</h1>
+        {reactTable(8,8,updateStateFunction)}
         </div>
     )
 }
